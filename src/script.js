c38752026854e1bfce80b597ccda9726596bb304
aaronERA <43649938+aaronERA@users.noreply.github.com>
2025-11-24 23:11:50 -0800

    trash concept. Delete src directory
    shrug-- wasnt feeling the idea of a cube being the UI. jsut give me info, you know????

--- src/script.js
+++
@@ -1,6 +0,0 @@
-const cube = document.querySelector('.cube');
-let float = 0;
-setInterval(() => {
-  float = Math.sin(Date.now() / 500) * 5;
-  cube.style.transform = `translateY(${float}px)`;
-}, 30);
